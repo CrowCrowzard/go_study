8812999265fac36bf3f15968d15621f0fcbdfeea
CrowCrowzard <crowizard@gmail.com>
2016-10-29 11:24:56 +0900

    package study, after split

--- go-package/main.go
+++ go-package/main.go
@@ -1,17 +1,9 @@
 package main
 
-import "log"
+import "./foobar"
 
 func main() {
-    foo()
-    bar()
-}
-
-func foo() {
-    log.Print("Hello world from foo!")
-}
-
-func bar() {
-    log.Print("Hello world from bar!")
+    foobar.Foo()
+    foobar.Bar()
 }
 
